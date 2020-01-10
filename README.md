7a00153204531717af0c872373cea0713c65712a
carlosrrod <carlosrrdev@gmail.com>
2020-01-09 19:46:45 -0600

    Update README, words.txt, and script files

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 Python Password Generator
 
 ## Description:
-Using words found inside the words.txt file, this script takes a random word and injects numbers and special characters in order to create a complete random string or characters. The user can decide the lenght of the password
+Generate a strong password using a combination of special characters and numbers. Passwords are automatically stored in a local txt file.
 
+## Usage
+This script selects from a random word, you can alter which words the script chooses from by editing the words.txt file. Currently, it is using [Bacon Ipsum](https://baconipsum.com/) but you can type in any words you'd like, just make sure they are all in a single line with words seperated by spaces.
--- passgen.py
+++ passgen.py
@@ -2,7 +2,7 @@
 # Version 1.0
 
 # Import packages
-import string, random
+import random
 
 # Get a random word from words.txt
 def get_word():
@@ -11,26 +11,47 @@ def get_word():
     Modify: You can add/delete words as needed, must be on a single
     line and seperated by single spaces. 
     '''
+    # Store words in local variale
     words = open('words.txt', 'r')
     line = words.readline()
-
-    rand_num = random.randint(0,len(line.split()))
-
+    # Seperate words using spaces
+    rand_num = random.randint(0,len(line.split())-1)
+    # Return a randomly selected word
     return line.split()[rand_num]
 
 # Build password
-def build_pass(pass_len=5):
+def build_pass():
     '''
     Purpose: Build a string using a selected word and injecting
     numbers and special characters
     '''
+    # 1. Retrieve a random work from the words.txt file
     random_word = get_word()
-    special_chars = ['!', '@', '#', '$', '%', '&', '*']
+    # 2. Setup special characters and numbers
+    special_chars = ['!', '@', '#', '$', '%', '&', '*', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    # 3. Setup empty string as a placeholder for new password
     new_pass = ''
+    # 4. Loop through every character in random_word
     for letter in random_word:
-        rand_num = random.randint(0, 6)
+        pass_len = len(new_pass)
+        special_len = len(special_chars)
+        # Create random numbers
+        rand_num1 = random.randint(0, special_len)
+        rand_num2 = random.randint(0, pass_len)
+        # 5. Inject special characters or numbers into the new password
         new_pass += letter
-        new_pass += special_chars[rand_num]
-    print(new_pass)
+        new_pass += special_chars[rand_num1-1]
 
-build_pass()
+        # 6. Capatalize a random letter
+        new_pass[rand_num2].upper()
+
+    return new_pass
+
+def write_pass():
+    '''
+    Purpose: Save the newly created password into a local txt file
+    '''
+    with open('saved_passwords.txt', 'a') as log:
+        log.write(f'Saved password: {build_pass()}\n')
+
+write_pass()
